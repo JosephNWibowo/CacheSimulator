23fb8c20ae3c2fb9ae9807aad92df24bcefbe543
josephnwibowo <jwibowo@mail.sfsu.edu>
2017-04-28 15:28:33 -0700

    put another one

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.print("testing git");
-        System.out.println();
+        System.out.println("another one");
+
     }
 }
