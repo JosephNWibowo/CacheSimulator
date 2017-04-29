b97d9d4620f5efdba2ae69b62c8a702092f12b7a
josephnwibowo <jwibowo@mail.sfsu.edu>
2017-04-28 17:00:29 -0700

    implementing scanner file

--- src/main.java
+++ src/main.java
@@ -1,10 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by Joey on 4/28/2017.
  */
 public class main {
-    public static void main(String[] args) {
-        System.out.print("testing git");
-        System.out.println("another one");
+    public static void main(String[] args) throws IOException {
+       int dataReads,
+            dataWrites,
+            dataAccess,
+            totDataReadMisses,
+            totDataWriteMisses,
+            numDataMisses;
+
+
+        String path = args[0];                          //taking arguments as the file path
+        Scanner txtFile = new Scanner(new File(path));  //put the file to be scanned
+
+
+        while (txtFile.hasNext()) {
+            String[] lineSplit = txtFile.nextLine().split(" :");
+            for(int i = 0; i < lineSplit.length; i++) {
+                System.out.println(lineSplit[i] + " ");
+            }
+            
+        }
+
+
 
     }
 }
