821eb45ea3daa06522b1cdfc42e4d70377118256
josephnwibowo <jwibowo@mail.sfsu.edu>
2017-05-03 20:54:09 -0700

    splitting binary input to index, and tag

--- src/line.java
+++ src/line.java
@@ -1,7 +1,50 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Joey on 5/3/2017.
  */
 public class line {
+    boolean validBit = false;
+    String binary;
+    String indexBin;
+    String tagBin;
+
+
+    public line(String binaryInput, int offset, int indexBits, int tagBits) {
+        binary = binaryInput;
+        int startBinIndex = binaryInput.length() - (offset + indexBits);
+        int endBinIndex = binaryInput.length() - offset;
+        indexBin = binaryInput.substring(startBinIndex, endBinIndex);
+        tagBin = binaryInput.substring(0, startBinIndex);
+    }
+
+    public boolean isValidBit() {
+        return validBit;
+    }
+
+    public void setValidBit(boolean validBit) {
+        this.validBit = validBit;
+    }
+
+    public String getBinary() {
+        return binary;
+    }
+
+    public void setBinary(String binary) {
+        this.binary = binary;
+    }
+
+    public String getIndexBin() {
+        return indexBin;
+    }
+
+    public void setIndexBin(String indexBin) {
+        this.indexBin = indexBin;
+    }
+
+    public String getTagBin() {
+        return tagBin;
+    }
+
+    public void setTagBin(String tagBin) {
+        this.tagBin = tagBin;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -10,6 +11,9 @@ public class main {
         int dataReads = 0;
         int dataWrites = 0;
         int dataAccess;
+        int byteBlocks = 16;
+        int bitMemAddress = 32;
+        int offset;
         int totDataReadMisses;
         int totDataWriteMisses;
         int numDataMisses;
@@ -20,11 +24,25 @@ public class main {
         int totReadAccessTime;
         int totWriteAccessTime;
         int overallDataCacheMissRate;
+        String tempString;
+        line tempLine;
 
 
         String path = args[0];                                              //taking arguments as the file path
         Scanner txtFile = new Scanner(new File(path));                      //put the file to be scanned
 
+        String StringCacheSize = args[1];                                   //taking the 2nd arguments as the cache size
+        int intCacheSize = 1024 * (Integer.parseInt(StringCacheSize));      //converting it into integer and multiply w/
+                                                                            //1kb = 1024 bytes
+
+        offset = (int) (Math.log(byteBlocks) / Math.log(2));                //logBase2 the byteblocks to get offset
+
+        int numBlocks = intCacheSize / byteBlocks;                          //#blocks in cache
+        int numIndexes = (int) (Math.log(numBlocks) / Math.log(2));         //logbase2(#blocks) to get the index
+        int tagBits = bitMemAddress - (numIndexes + offset);                //get tag by subtracting 32 -(index+offset)
+
+
+        ArrayList<line> cache = new ArrayList<line>();
 
         while (txtFile.hasNext()) {
             String[] lineSplit = txtFile.nextLine().split("[: ]+");  //to remove colon and  multiple spaces
@@ -34,16 +52,52 @@ public class main {
             } else {
                 dataReads++;
             }
-           /* for(int i = 0; i < lineSplit.length; i++) {
-                System.out.println(lineSplit[i]);
-            }*/
+
+
+            tempString = lineSplit[2].substring(2);
+            tempLine = new line(hexToBinary(tempString), offset, numIndexes, tagBits);
+            System.out.println(hexToBinary(tempString));
+
+            System.out.println(tempLine.getIndexBin());
+            System.out.println(tempLine.getTagBin());
+
+
+            /*System.out.println(lineSplit[2].substring(2));
+            tempString = lineSplit[2].substring(2);
+            System.out.println(hexToBinary(tempString));*/
+
         }
 
         dataAccess = dataReads + dataWrites;
 
-        System.out.println("number of data reads:\t\t" + dataReads
-                + "\nnumber of data writes:\t\t" + dataWrites
-                + "\nnumber of accesses:\t\t\t" + dataAccess);
+        System.out.println("number of data reads:\t" + dataReads
+                + "\nnumber of data writes:\t" + dataWrites
+                + "\nnumber of accesses:\t\t" + dataAccess);
+
+
+        System.out.println("\noffset:\t\t\t\t\t" + offset
+                + "\nnumber of lines:\t\t" + numBlocks
+                + "\nnumber of indexes:\t\t" + numIndexes
+                + "\ntag bits:\t\t\t\t" + tagBits);
 
     }
+
+    public static String hexToBinary(String s)
+    {
+        String digits = "0123456789ABCDEF";
+        String binary;
+        s = s.toUpperCase();
+        int val = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val + d;
+        }
+         binary = Integer.toBinaryString(val);
+        return binary;
+    }
+
 }
+
